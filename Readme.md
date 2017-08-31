ee97a4a38167d0170d512484ccc1ee263551e795
makevoid <makevoid@gmail.com>
2017-09-01 00:30:34 +0100

    remove rollup in favour of browserify and buble instead of babel

--- Readme.md
+++ Readme.md
@@ -30,3 +30,10 @@ or:
 Please require `dist/b-privacy.iife.js`, that's the browser-ready version.
 
 You also need to include `bitcore-lib` manually before that. See `test/browser.index.html` for a working example.
+
+
+To make the example work you need to build bitcore-lib for the browser, run:
+
+    cd node_modules/bitcore-lib
+    npm i
+    gulp browser
--- src/b-privacy.js
+++ src/b-privacy.js
@@ -111,6 +111,8 @@ class BPrivacy {
 module.exports = BPrivacy
 
 
+if (process.browser) window.BPrivacy = BPrivacy
+
 // *note1: 16 bytes (128 bits) seed for a 12 words seed - note: a value higher than that doesn't provides additional security because 256 bit keys of bitcoin are only 128 bits strong
 
 // *note2: this constructs a mnemonic path level - here are some infos on path levels: https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#path-levels - note we skip the change address bit as we don't usually deal with native chain tokens (test-eths) as we run PoA as consensus algo

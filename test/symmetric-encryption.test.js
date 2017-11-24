cf8366f4e30a2c5bd453920ba61417ab4ce0ed51
Mirek Rusin <mirek@me.com>
2017-11-24 06:58:00 +0000

    Adding symmetric encryption.
    This should be ruby compatible. The only difference is that we want to
    work on single blobs of data so cipher output/decipher input are blobs
    of data with iv (16 bytes) + encoded data.

--- test/symmetric-encryption.test.js
+++ test/symmetric-encryption.test.js
@@ -1,8 +1,18 @@
 const assert = require('assert');
-const { isHex } = require('../src/core');
+const { encrypt, decrypt } = require('../src/symmetric-encryption');
+const crypto = require('crypto');
+
+function t(a, b, c) {
+  return assert.deepEqual(a, b, c);
+}
 
 describe('Symmetric encryption', function () {
-  it('should recognise hex string', function () {
-    assert(isHex('0303f0125c59df0c93b5a20c90fd7924308f6ef5b61bc5704cdade2c53c6941952'));
+
+  it('should work', function () {
+    const secret = crypto.randomBytes(32);
+    const encrypted = encrypt({ foo: 'bar' }, secret);
+    const decrypted = decrypt(encrypted, secret);
+    t(decrypted, { foo: 'bar' });
   });
+
 });

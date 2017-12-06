69fc16ad1f2c3231c0a7f55c9151d029970e04f3
Mirek Rusin <mirek@me.com>
2017-12-06 11:39:22 +0000

    Moving symmetric encrypt/decrypt into core.

---
+++ src/core/decrypt-symmetric.js
@@ -0,0 +1,33 @@
+
+const crypto = require('crypto')
+const bytesToBuffer = require('./bytes-to-buffer')
+
+/**
+ * Decrypts `blob_` using `secret_` and symmetric `.algo` (default aes-256-cbc).
+ *
+ * @param {} blob_ 16-bytes iv + encrypted data.
+ * @param {bytes} secret_ 32 bytes secret.
+ * @param {string} .algo = 'aes-256-cbc'
+ * @return {any} Decrypted value.
+ */
+function decryptSymmetric(blob_, secret_, { algo = 'aes-256-cbc' } = {}) {
+  const secret = bytesToBuffer(secret_)
+  if (secret.length !== 32) {
+    throw new TypeError(`Expected 32 bytes long secret, got ${secret.length}.`)
+  }
+  const blob = bytesToBuffer(blob_)
+  if (blob.length <= 16) {
+    throw new TypeError(`Expected blob with length greater than 16, got ${blob.length}.`)
+  }
+  const iv = blob.slice(0, 16)
+  const encrypted = blob.slice(16)
+  const decipher = crypto.createDecipheriv(algo, secret, iv)
+  const decrypted = Buffer.concat([
+    decipher.update(encrypted),
+    decipher.final()
+  ])
+  const json = JSON.parse(decrypted.toString('utf8'))
+  return json
+}
+
+module.exports = decryptSymmetric
---
+++ src/core/encrypt-symmetric.js
@@ -0,0 +1,32 @@
+
+const crypto = require('crypto')
+const randomBytes = require('./random-bytes')
+const bytesToBuffer = require('./bytes-to-buffer')
+
+/**
+ * Encrypts `value` using symmetric encryption `.algo` with provided `secret`.
+ *
+ * @param {any} value Any JSON serializable value.
+ * @param {bytes} secret_ 32 bytes secret, ie. `randomBytes(32)`.
+ * @return {buffer} 16-bytes iv followed by encrypted blob.
+ */
+function encryptSymmetric(value, secret_, { algo = 'aes-256-cbc' } = {}) {
+  const secret = bytesToBuffer(secret_)
+  if (secret.length !== 32) {
+    throw new TypeError(`Expected 32 bytes long secret, got ${secret.length}.`)
+  }
+  const json = JSON.stringify(value)
+  const iv = randomBytes(16)
+  const cipher = crypto.createCipheriv(algo, secret, iv)
+  const encrypted = Buffer.concat([
+    cipher.update(json),
+    cipher.final()
+  ])
+  const blob = Buffer.concat([
+    iv,
+    encrypted
+  ])
+  return blob
+}
+
+module.exports = encryptSymmetric

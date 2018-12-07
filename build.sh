dd7494e67c476b3688169768ae5c6ba336089e8e
Alaan Rahim <alaan@appliedblockchain.com>
2018-12-07 14:04:33 +0000

    Add client publication to circleci config

--- build.sh
+++ build.sh
@@ -7,4 +7,3 @@ cp package.json ./dist
 cd ./dist
 sed -i '-backup' 's/"name": "@appliedblockchain\/b-privacy",/"name": "@appliedblockchain\/b-privacy-client",/g' package.json
 rm package.json-backup
-npm i
